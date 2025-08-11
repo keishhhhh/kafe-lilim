ce09ece5f783b2424c6a048c7b00b6c08011d01c
keishh <124478818+keishhhhh@users.noreply.github.com>
2025-08-12 07:43:48 +0800

    Create use-toast.js

---
+++ src/components/ui/use-toast.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.toast = exports.useToast = void 0;
+var use_toast_1 = require("@/hooks/use-toast");
+Object.defineProperty(exports, "useToast", { enumerable: true, get: function () { return use_toast_1.useToast; } });
+Object.defineProperty(exports, "toast", { enumerable: true, get: function () { return use_toast_1.toast; } });
